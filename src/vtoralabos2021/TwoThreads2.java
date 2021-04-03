dff53dd491b8efb02901ff2a5923f41568f0f676
Mgiooa <martina.gligorova1@gmail.com>
2021-04-03 18:31:47 +0200

    Revision

--- src/vtoralabos2021/TwoThreads2.java
+++ src/vtoralabos2021/TwoThreads2.java
@@ -10,13 +10,7 @@ public class TwoThreads2 {
 
 
     public static void main(String[] args) throws InterruptedException, IOException {
-      /*  ThreadClassLetters letters = new ThreadClassLetters();
-        ThreadClassNumbers numbers = new ThreadClassNumbers();
-        letters.start();
-        letters.join();
-        numbers.start();
-        numbers.join();
-       */
+
         List<Object> lista = new ArrayList<>();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String s = "";
@@ -30,7 +24,6 @@ public class TwoThreads2 {
         lettersNumbers.join();
         //   System.out.println("Succesful!");
     }
-
 }
 
 class ThreadClassLettersNumbers2 extends Thread {
@@ -44,34 +37,9 @@ class ThreadClassLettersNumbers2 extends Thread {
 
     @Override
     public void run() {
-    /*    for (int i = 0; i < 10; i++) {
-            System.out.println((char) i + 65);
-        }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-
-     */
         for (int i = 0; i < lettersnumbers.size(); i++) {
             System.out.println(lettersnumbers.get(i));
         }
     }
 }
-/*class ThreadClassNumbers extends Thread {
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) System.out.println(i);
-    }
-}
-
-
-class ThreadClassLetters extends Thread {
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) System.out.println((char) (i + 65));
-    }
-}
- */
